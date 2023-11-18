6b3199c316db5669adab1d341e2523ead06ec0c3
Wyeben <yila.ben@yahoo.com>
2023-11-18 06:38:38 +0100

    additional code

---
+++ exercise/EmailSender.js
@@ -0,0 +1,8 @@
+const fullName = document.getElementById('fullName');
+const title = document.getElementById('title');
+const sender = document.getElementById('senderEmail');
+const content = document.getElementById('content');
+
+function senderEmail(){
+    
+}
